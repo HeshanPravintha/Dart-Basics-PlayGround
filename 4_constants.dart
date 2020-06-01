9a3e00eb80540a571d8a05b3638021826717aff4
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2020-06-01 22:15:12 +0530

    Initial commit

---
+++ 4_constants.dart
@@ -0,0 +1,19 @@
+
+void main() {
+
+	// final
+	final cityName = 'Colombo';
+	//	name = 'Heshan';     // Throws an error
+
+	final String countryName = 'Sri Lanka';
+
+	// const
+	const PI = 3.14;
+	const double gravity = 9.8;
+}
+
+class Circle {
+
+	final color = 'Red';
+	static const PI = 3.14;
+}
